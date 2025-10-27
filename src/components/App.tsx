4f9dd442994f966355cf161291e8fa2b7de8bdd6
Simplice-Kolomou <s.kolomou@student.helmo.be>
2025-10-27 16:44:55 +0100

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import '../styles/App.css'
+import {Heading} from "@chakra-ui/react";
+
+function App() {
+
+  return (
+      <div className="App">
+          <h1>Hello world!</h1>
+          <Heading>
+              Hello, Chakra UI with Vite and React!
+          </Heading>
+      </div>
+  )
+}
+
+export default App
