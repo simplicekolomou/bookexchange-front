9139fe0a3daaa5e8633371d4684e6610fa50cd06
Theo A. <t.andernack@student.helmo.be>
2025-11-03 11:13:51 +0100

    Le logo est maintenant un lien

--- src/components/layout/LogoWithText.tsx
+++ src/components/layout/LogoWithText.tsx
@@ -1,5 +1,6 @@
 import { Box, Text, Flex } from "@chakra-ui/react";
 import { BookOpen } from "lucide-react";
+import {useNavigate} from "react-router-dom";
 
 interface LogoWithTextProps {
     title: string;
@@ -7,14 +8,20 @@ interface LogoWithTextProps {
 }
 
 export const LogoWithText = ({ title, direction = "row" }: LogoWithTextProps & { direction?: "row" | "column" }) => {
+    const navigate = useNavigate();
+    const goto = (link: string) => {
+        navigate(link);
+    };
     return (
-        <Flex className="logo-with-text" flexDirection={direction} alignItems="center">
-            <Box className="logo-box" padding={0}>
-                <BookOpen color="white" />
-            </Box>
-            <Box mt={direction === "column" ? 2 : 0} ml={direction === "row" ? 2 : 0} padding={0}>
-                <Text className="logo-title">{title}</Text>
-            </Box>
-        </Flex>
+        <a onClick={() => goto("/")} style={{ textDecoration: "none", cursor: "pointer" }}>
+            <Flex className="logo-with-text" flexDirection={direction} alignItems="center">
+                <Box className="logo-box" padding={0}>
+                    <BookOpen color="white" />
+                </Box>
+                <Box mt={direction === "column" ? 2 : 0} ml={direction === "row" ? 2 : 0} padding={0}>
+                    <Text className="logo-title">{title}</Text>
+                </Box>
+            </Flex>
+        </a>
     );
 };
