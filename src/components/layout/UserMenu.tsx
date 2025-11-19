efb83248b39a8ecc53bef416282a758c36f37a1e
Theo A. <t.andernack@student.helmo.be>
2025-11-19 14:58:18 +0100

    Correction d'une clé pour I18n

--- src/components/layout/UserMenu.tsx
+++ src/components/layout/UserMenu.tsx
@@ -32,7 +32,7 @@ export const UserMenu = () => {
                         <Menu.Item value="profile">
                             <Link to={"/profile"}>
                                 <User className="user-menu-icon" />
-                                {tLocal("profile")}
+                                {tLocal("profil")}
                             </Link>
                         </Menu.Item>
 
