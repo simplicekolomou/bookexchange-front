1d44879c37e2a488b552404e6e14b88dab208722
simplicekolomou <s.kolomou@student.helmo.be>
2026-06-18 19:23:51 +0200

    adaptation pour récupérer maintenant un token ws depuis le backend

--- src/features/auth/api/authApi.ts
+++ src/features/auth/api/authApi.ts
@@ -192,7 +192,7 @@ export const authApi = baseApi.injectEndpoints({
 
         getWebsocketToken: builder.query<string, void>({
             query: () => ({
-                url: `/api/ws-token`,
+                url: `/ws-token`,
                 method: 'GET',
             }),
             providesTags: ['WsToken'],
