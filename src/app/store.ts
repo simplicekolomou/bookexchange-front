507f2218d32f89173358cbdbff8a609028b0b674
simplicekolomou <s.kolomou@student.helmo.be>
2026-07-03 02:47:09 +0200

    correction du store

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,12 +3,14 @@ import { setupListeners } from '@reduxjs/toolkit/query'
 import { baseApi } from '../services/baseApi.ts'
 import messageReducer from "../features/message/messageSlice.ts";
 import authReducer from "../features/auth/authSlice.ts";
+import bookReducer from "../features/book/bookSlice.ts";
 
 export const store = configureStore({
     reducer: {
         [baseApi.reducerPath]: baseApi.reducer,
         message: messageReducer,
         auth: authReducer,
+        book: bookReducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(baseApi.middleware),
